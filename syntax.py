2c9906965f2b9877eedddf3950499dd5402d3759
Alwi <62659068+yaelahwi@users.noreply.github.com>
2022-10-02 13:12:38 +0700

    Update syntax.py
    hehe

--- syntax.py
+++ syntax.py
@@ -2,3 +2,7 @@ if 5 > 2:
  print("Five is greater than two!") 
 if 5 > 2:
         print("Five is greater than two!") 
+
+def setup_method(self, method):
+   self.driver = webdriver.Chrome()
+   self.vars = {}
